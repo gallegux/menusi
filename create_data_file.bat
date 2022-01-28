d6a1fa9a51d8dffb053671c73bb5593ae2484402
gallegux <37558704+gallegux@users.noreply.github.com>
2022-01-28 11:47:39 +0100

    arguments

--- create_data_file.bat
+++ create_data_file.bat
@@ -1,4 +1,12 @@
 @echo off
 
-java csv2dat menusi_data.csv temp_file
-java AddAmsdosHeader menusi.dat temp_file menusi.dat
+rem %1 CSV file
+rem %2 DAT file for Amstrad CPC
+
+rem java csv2dat menusi_data.csv temp_file
+rem java AddAmsdosHeader menusi.dat temp_file
+
+java csv2dat %1 temp_file
+java AddAmsdosHeader %2 temp_file
+
+del temp_file
