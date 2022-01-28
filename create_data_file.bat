98453343dc696d143df56369dd4a7e9230479d28
gallegux <37558704+gallegux@users.noreply.github.com>
2022-01-28 19:45:10 +0100

    arguments

--- create_data_file.bat
+++ create_data_file.bat
@@ -5,6 +5,7 @@ rem %2 DAT file for Amstrad CPC
 
 rem java csv2dat menusi_data.csv temp_file
 rem java AddAmsdosHeader menusi.dat temp_file
+echo Arguments: <csv file> <dat file for amstrad cpc (8 characteres)>
 
 java csv2dat %1 temp_file
 java AddAmsdosHeader %2 temp_file
