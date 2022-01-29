58fbbbd3a1c72b0360fd4a9d6d415610a48f88d4
gallegux <37558704+gallegux@users.noreply.github.com>
2022-01-29 13:37:14 +0100

    Add files via upload

--- create_data_file.bat
+++ create_data_file.bat
@@ -3,11 +3,21 @@
 rem %1 CSV file
 rem %2 DAT file for Amstrad CPC
 
-rem java csv2dat menusi_data.csv temp_file
-rem java AddAmsdosHeader menusi.dat temp_file
-echo Arguments: <csv file> <dat file for amstrad cpc (8 characteres)>
+if "%1"=="" goto help
+if "%2"=="" goto help
+
+rem echo %1
+rem echo %2
 
 java csv2dat %1 temp_file
 java AddAmsdosHeader %2 temp_file
-
 del temp_file
+goto fin
+
+:help
+
+echo Arguments: csv_file dat_file
+echo    csv_file - the csv file with programs and orders
+echo    dat_file - the data file for amstrad cpc (8+3)
+
+:fin
